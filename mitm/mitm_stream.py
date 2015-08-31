88ac53e99c389fdea284d1e91c2dc34a0a006efb
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2015-08-31 09:11:47 -0600

    Both request and response must be taken into account. No local modules are to be loaded within the mitm_stream inline script.

--- mitm/mitm_stream.py
+++ mitm/mitm_stream.py
@@ -3,13 +3,29 @@ from libmproxy.flow import FlowWriter
 from urllib import unquote
 from datetime import datetime
 
-from router import redirect
-
 '''
 Reads responses from MITM proxy and maps relevant info to a dict.
 Then calls the router to redirect HTTPResponse dict.
 '''
 def response(context, flow):
+    #Process request
+    requestAttrs = ["method", "scheme", "host", "port", "path", "httpversion", "headers", "content", "form_in", "timestamp_start", "timestamp_end"]
+    
+    requestDict = {}
+    
+    requestDict['date-sent'] = datetime.now()
+    #Build HTTPRequest dict with decoded data
+    with decoded(flow.request):
+        for attr in requestAttrs:
+            try:
+                if attr == "content":
+                    requestDict[attr] = unquote(flow.request.content)
+                else:
+                    requestDict[attr] = str(getattr(flow.request, attr))
+            except Exception, e:
+                requestDict[attr] = str(e)
+    
+    #Process response
     responseAttrs = ["httpversion", "status_code", "msg", "headers", "content", "timestamp_start", "timestamp_end"]
     
     responseDict = {}
@@ -19,12 +35,17 @@ def response(context, flow):
     with decoded(flow.response):
         for attr in responseAttrs:
             try:
-                if attr == "headers":
-                    responseDict[attr] = flow.response.headers
                 if attr == "content":
                     responseDict[attr] = unquote(flow.response.content)
                 else:
                     responseDict[attr] = str(getattr(flow.response, attr))
             except Exception, e:
                 responseDict[attr] = str(e)
-    redirect.redirect(responseDict)
+    
+#    print redirectTo(requestDict, responseDict)
+
+'''
+This method receives an HTTPRequest dict and a HTTPResponse dict, reads its headers and redirects it to the corresponding processor. 
+'''
+def redirectTo(request, response):
+    pass
