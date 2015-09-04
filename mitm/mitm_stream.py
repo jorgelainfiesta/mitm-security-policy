5bef752be8411d39eaec70b167f791df4c2052a8
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2015-09-03 20:45:24 -0600

    Receive endpoint host as parameter for the inline script. Transform content to unicode in order to transform it to JSON.

--- mitm/mitm_stream.py
+++ mitm/mitm_stream.py
@@ -3,6 +3,14 @@ from libmproxy.flow import FlowWriter
 from urllib import unquote
 from datetime import datetime
 
+import requests
+import json
+
+def start(context, argv):
+    if len(argv) != 2:
+        raise ValueError('Usage: -s "mitm_stream.oy endpoint"')
+    context.endpoint = argv[1]
+    
 '''
 Reads responses from MITM proxy and maps relevant info to a dict.
 Then calls the router to redirect HTTPResponse dict.
@@ -13,13 +21,13 @@ def response(context, flow):
     
     requestDict = {}
     
-    requestDict['date-sent'] = datetime.now()
+    requestDict['date-sent'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     #Build HTTPRequest dict with decoded data
     with decoded(flow.request):
         for attr in requestAttrs:
             try:
                 if attr == "content":
-                    requestDict[attr] = unquote(flow.request.content)
+                    requestDict[attr] = unicode(unquote(flow.request.content), "utf-8")
                 else:
                     requestDict[attr] = str(getattr(flow.request, attr))
             except Exception, e:
@@ -30,22 +38,27 @@ def response(context, flow):
     
     responseDict = {}
     
-    responseDict['date-received'] = datetime.now()
+    responseDict['date-received'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     #Build HTTPResponse dict with decoded data
     with decoded(flow.response):
         for attr in responseAttrs:
             try:
                 if attr == "content":
-                    responseDict[attr] = unquote(flow.response.content)
+                    responseDict[attr] = unicode(unquote(flow.response.content), "ISO-8859-1")
                 else:
                     responseDict[attr] = str(getattr(flow.response, attr))
             except Exception, e:
                 responseDict[attr] = str(e)
+                
+    #Redirect messages to appropaite destinations
+    redirect(requestDict, responseDict, context.endpoint)
     
-#    print redirectTo(requestDict, responseDict)
-
 '''
 This method receives an HTTPRequest dict and a HTTPResponse dict, reads its headers and redirects it to the corresponding processor. 
 '''
-def redirectTo(request, response):
+def redirect(request, response, endpoint):
+    #Send test request
+#    r = requests.post(endpoint, data=json.dumps(requestDict))
+#    print r.text
+    
     pass
