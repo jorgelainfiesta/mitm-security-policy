c65c40d046955dca1e796616fdea61ff9836a4d9
Ale GnZl <gon11220@gmail.com>
2015-09-21 13:17:05 -0600

    Obtención de tildes exitosamente
    Tildes de subject y predicado funcionando correctamente.

--- mitm/translator_service.py
+++ mitm/translator_service.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-<2
 import requests
 import parsers.parser as prs
 import json
@@ -14,6 +14,18 @@ import cgi
 available_queues = ["error"]
 queues = {q: Queue.Queue() for q in available_queues}
 
+def change(subject):
+    enc = ["\u00e1","\u00e9","\u00ed","\u00f3","\u00fa","\u00c1","\u00c9","\u00cd","\u00d3","\u00da","\u00f1","\u00d1"]
+    dec = ["á","é","í","ó","ú","Á","É","Í","Ó","Ú","ñ","Ñ"]
+
+    index = 0
+    for rep in enc:
+        subject = subject.replace(rep,dec[index])
+        index += 1
+
+    return subject
+
+
 def translate(url_base='http://localhost:9090'):
     print "    Iniciando servicio de traduccion de mensajes EndPoint"
     print "        mitmProxy - Parsers - Analizador..."
@@ -23,7 +35,8 @@ def translate(url_base='http://localhost:9090'):
         try:
             #Consultar cola mailOut
             r = requests.get(url_base + '/get?queue=mailOut')
-            contentData = eval(r.text)
+            contentData = eval(r.text) #quiero ver todo lo que devuelve esto, me refiero a la ahh hahah
+           # print  contentData.keys()    
         except Exception, err:
             print "    # Error: # error al intentar obtener mensajes."
             print '               %sn' % str(err)
@@ -45,20 +58,26 @@ def translate(url_base='http://localhost:9090'):
                         print content
                         print ""
                         content = content["content"].encode("utf-8")
+                        #subject =  content["subject"].encode("utf-8")
+                        print content
+                        #       subject =  subject["subject"].encode["utf-8"]
                         p = prs.Parser()
                         sender, recipients, subject, body = p.contentParser(content)
-                        
-                        subject = subject.decode('utf8')
+                         
+                        print subject
+                        #subject = subject.decode('utf8')
                         h = HTMLParser.HTMLParser()
-                        subject = h.unescape(cgi.escape(subject).encode('ascii', 'xmlcharrefreplace'))
-                        print "sujeto: " , subject
+                        subject = h.unescape(subject) 
+                        subject = change(subject)
+                        print "sujeto: " , subject      
 
+        
                         body = body.decode('utf8')
                         body = h.unescape(body)
                         print "predicado: ", body
  
                         sender = "neryalecorp@gmail.com" # Para el día sábado 19 de septiembre de 2015, el emisor ya no aparece en la data capturada.
-                        print "        correo: Enviado por " + sender + " para " + str(recipients) + " con asunto " + subject + ". " + body 
+                        print "        correo: Enviado por " + sender + " para " + str(recipients) + " con asunto " + ". " + body 
                         #break
                         #cadena = u"día niño áéíóú".encode('latin_1')
                         #cadena = "día niño áéíóú"
